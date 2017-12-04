fb49235a49ad9406821ce369c17b5815b222b7ec
HumnaRasheed <hrasheed@unomaha.edu>
2017-12-04 12:35:42 -0600

    Humna

---
+++ Tests/HR-Midcity-EventSignup.py
@@ -0,0 +1,37 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class Event_MCP(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_event(self):
+        user = "instructor"
+        pwd = "instructor1a"
+        driver = self.driver
+        driver.maximize_window()
+        driver.get("http://midcityproject.pythonanywhere.com/")
+        time.sleep(5)
+        elem = driver.find_element_by_xpath("/html/body/nav/div/ul[2]/li/a[2]").click()  #sign in
+        elem = driver.find_element_by_name("username")
+        elem.send_keys(user)
+        elem = driver.find_element_by_name("password")
+        elem.send_keys(pwd)
+        time.sleep(5)
+        elem.send_keys(Keys.RETURN)
+
+        #elem = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/form/input[2]").click() #login
+        driver.get("http://midcityproject.pythonanywhere.com/")
+        time.sleep(5)
+        elem = driver.find_element_by_xpath("/html/body/div[2]/div[3]/a").click() #view all events
+        driver.get("http://midcityproject.pythonanywhere.com/events/")
+        time.sleep(8)
+        elem = driver.find_element_by_xpath("/html/body/div[2]/div/div[7]/div/div[2]/a[2]").click() #event signup
+        time.sleep(5)
+        elem = driver.find_element_by_xpath("/html/body/form/button").click()#save button
+        time.sleep(5)
+        def tearDown(self):
+            self.driver.close()
---
+++ Tests/HR-Midcity-Register.py
@@ -0,0 +1,45 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class Reg_MCP(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_mcp(self):
+        user = "AbeerM"
+        fname = "Abeer"
+        mail = "barun120@uno.com"
+        pwd1 = "arun100"
+        pwd2 = "arun100"
+        driver = self.driver
+        driver.maximize_window()
+        driver.get("http://midcityproject.pythonanywhere.com/")
+        time.sleep(5)
+        elem = driver.find_element_by_xpath("/html/body/nav/div/ul[2]/li/a[1]").click()
+        driver.get("http://midcityproject.pythonanywhere.com/register/")
+        time.sleep(5)
+        elem = driver.find_element_by_name("username")
+        elem.send_keys(user)
+        elem = driver.find_element_by_name("first_name")
+        elem.send_keys(fname)
+        elem = driver.find_element_by_name("email")
+        elem.send_keys(mail)
+        elem = driver.find_element_by_name("password")
+        elem.send_keys(pwd1)
+        elem = driver.find_element_by_name("password2")
+        elem.send_keys(pwd2)
+        time.sleep(6)
+        elem.send_keys(Keys.RETURN)
+
+        #elem = driver.find_element_by_xpath("/html/body/div[2]/div/div/input").click()
+        #time.sleep(2)
+        #driver.get("http://midcityproject.pythonanywhere.com/register/")
+        assert "registered!"
+        time.sleep(5)
+
+        def tearDown(self):
+            self.driver.close()
+
